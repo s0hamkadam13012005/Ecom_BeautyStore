3bb3969f08328927358e3b7febefedcbebc2d40d
Soham <sohamkadam1844@gmail.com>
2025-11-30 13:43:39 +0530

    Added orders and Banners page

--- Admin/src/pages/Banners.jsx
+++ Admin/src/pages/Banners.jsx
@@ -1,9 +1,95 @@
-import React from 'react'
-
+import React from "react";
+import { FaPlus } from "react-icons/fa";
 const Banners = () => {
   return (
-    <div>Banners</div>
-  )
-}
+    <div className="flex justify-evenly m-[10%]">
+      {/* LEFT */}
+      <div className="mr-[50px]">
+        <h2 className="text-xl font-semibold mb-4">Active Banners</h2>
 
-export default Banners
+        <div className="flex flex-col space-y-4">
+          <div className="flex justify-between items-center border-b border-gray-200 pb-4">
+            <img
+              src="https://images.pexels.com/photos/8054395/pexels-photo-8054395.jpeg"
+              className="w-32 h-32 object-cover rounded-md"
+              alt=""
+            />
+
+            <div className="flex-1 ml-4">
+              <h3 className="text-xl font-semibold mb-2">
+                Radiate Beauty, Inside and Out.
+              </h3>
+              <p className="text-gray-300 mb-2">
+                Discover Your Perfect Products for a Flawless Look.
+              </p>
+              <button className="bg-red-500 font-semibold text-white p-2 cursor-pointer">
+                Delete
+              </button>
+            </div>
+          </div>
+
+          <div className="flex justify-between items-center border-b border-gray-200 pb-4">
+            <img
+              src="https://images.pexels.com/photos/8054395/pexels-photo-8054395.jpeg"
+              className="w-32 h-32 object-cover rounded-md"
+              alt=""
+            />
+
+            <div className="flex-1 ml-4">
+              <h3 className="text-xl font-semibold mb-2">
+                Radiate Beauty, Inside and Out.
+              </h3>
+              <p className="text-gray-300 mb-2">
+                Discover Your Perfect Products for a Flawless Look.
+              </p>
+              <button className="bg-red-500 font-semibold text-white p-2 cursor-pointer">
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* RIGHT */}
+
+      <div className="flex flex-col ">
+        <div className="flex-1 bg-white p-5 ">
+          <div className="flex flex-col ">
+            <span htmlFor="" className="font-semibold ">
+              Image:
+            </span>
+            <div className="border-2 h-[100px] w-[100px] border-[#444] border-solid rounded-md">
+              <div className="flex items-center justify-center mt-[40px]">
+                <label htmlFor="" className="cursor-pointer">
+                  <FaPlus className="text-[20px]" />
+                </label>
+              </div>
+            </div>
+
+            <div className="flex flex-col my-3">
+              <span className="font-semibold">Title:</span>
+              <input
+                type="text"
+                className="w-[250px] outline-none border-b-2 border-[#444] border-solid"
+              />
+            </div>
+
+            <div className="flex flex-col my-3">
+              <span className="font-semibold">Subtitle:</span>
+              <input
+                type="text"
+                className="w-[250px] outline-none border-b-2 border-[#444] border-solid"
+              />
+            </div>
+
+            <button className="bg-[#1e1e1e] p-2 text-white font-semibold cursor-pointer">
+              Upload
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banners;
--- Admin/src/pages/Orders.jsx
+++ Admin/src/pages/Orders.jsx
@@ -1,9 +1,93 @@
 import React from 'react'
+import { DataGrid } from "@mui/x-data-grid";
+import { FaCheckCircle, FaCheckDouble, FaClock } from "react-icons/fa";
+
 
 const Orders = () => {
-  return (
-    <div>Orders</div>
-  )
-}
+  const orders = [
+    {
+      _id: "0101",
+      name: "Alice Johnson",
+      email: "alice@example.com",
+      status: 1,
+    },
+    { _id: "0102", name: "Bob Smith", email: "bob@example.com", status: 0 },
+    {
+      _id: "0103",
+      name: "Charlie Brown",
+      email: "charlie@example.com",
+      status: 2,
+    },
+    { _id: "0104", name: "David Clark", email: "david@example.com", status: 1 },
+    { _id: "0105", name: "Eve Stone", email: "eve@example.com", status: 0 },
+    {
+      _id: "0106",
+      name: "Frank Wilson",
+      email: "frank@example.com",
+      status: 1,
+    },
+    { _id: "0107", name: "Grace Lee", email: "grace@example.com", status: 2 },
+    { _id: "0108", name: "Henry Kim", email: "henry@example.com", status: 0 },
+  ];
 
+  const columns = [
+    { field: "_id", headerName: "Order ID", width: 100 },
+    { field: "name", headerName: "Customer Name", width: 200 },
+    { field: "email", headerName: "Customer Email", width: 150 },
+    {
+      field: "status",
+      headerName: "Status",
+      width: 100,
+      renderCell: (params) => {
+        return (
+          <>
+            {params.row.status === 0 || params.row.status === 1 ? (
+              <FaClock className="text-yellow-500 text-[25px] cursor-pointer mt-2" />
+            ) : (
+              <FaCheckDouble className="text-green-500 text-[25px]" />
+            )}
+          </>
+        );
+      },
+    },
+    {
+      field: "Deliver",
+      headerName: "Mark as Delivered",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <>
+            {params.row.status === 1 || params.row.status === 0 ? (
+              <FaCheckCircle className=" text-[25px] cursor-pointer mt-2"
+              onClick={() => {
+               // handleUpdateOrder(params.row._id);
+              }}
+              />
+            ) : null}
+          </>
+        );
+      },
+    },
+  ];
+  return (
+     <div className="p-5 w-[70vw]">
+       <div className="flex justify-between items-center m-[30px]">
+         <h2 className="m-[20px] text-[20px] ">All Products</h2>
+ 
+         <button className="bg-[#1e1e1e] p-[10px] font-semibold text-white cursor-pointer">
+           Create
+         </button>
+       </div>
+       <div className="">
+         <DataGrid
+           getRowId={(row) => row._id}
+           rows={orders}
+           columns={columns}
+           checkboxSelection
+           autoHeight
+         />
+       </div>
+     </div>
+   );
+ };
 export default Orders
